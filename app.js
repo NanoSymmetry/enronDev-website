b64e8268dec63e6bc7243b57c45e094a4d2f7f10
NanoSymmetry <NanoSymmetry@users.noreply.github.com>
2022-04-30 18:01:27 -0400

    make the callback make sense to me

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 tsParticles.loadJSON('tsParticles', 'assets/particlesConfig.json').then(() => {
     // p is the loaded container, for using it later
-    console.log('callback - particles.js config loaded');
+    console.log('tsParticles config loaded');
   });

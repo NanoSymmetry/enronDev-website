0e4cc1e1d68afca039e1e81174cf139592bbbd9e
NanoSymmetry <NanoSymmetry@users.noreply.github.com>
2022-04-30 15:05:22 -0400

    move to v1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-tsParticles.loadJSON('particles-js', 'assets/particlesConfig.json').then(function(p) {
+tsParticles.load('tsparticles', 'assets/particlesConfig.json').then(function(p) {
     // p is the loaded container, for using it later
     console.log('callback - particles.js config loaded');
   });
